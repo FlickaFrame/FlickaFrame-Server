8ed892113e050cf7b1296c3271b7aed10c821d73
caicandong <1290147055@qq.com>
2023-10-25 14:26:10 +0800

    chore: feed api

--- internal/logic/feed/feed_logic.go
+++ internal/logic/feed/feed_logic.go
@@ -2,11 +2,13 @@ package feed
 
 import (
 	"context"
-
 	"github.com/FlickaFrame/FlickaFrame-Server/internal/svc"
 	"github.com/FlickaFrame/FlickaFrame-Server/internal/types"
-
+	"github.com/jinzhu/copier"
+	"github.com/qiniu/go-sdk/v7/auth/qbox"
+	"github.com/qiniu/go-sdk/v7/storage"
 	"github.com/zeromicro/go-zero/core/logx"
+	"time"
 )
 
 type FeedLogic struct {
@@ -24,8 +26,40 @@ func NewFeedLogic(ctx context.Context, svcCtx *svc.ServiceContext) *FeedLogic {
 }
 
 func (l *FeedLogic) Feed(req *types.FeedReq) (resp *types.FeedResp, err error) {
-	// 从数据库中获取feed
-	// TODO:
-	// 解析出feed然后从七牛云获取对象存储链接
+	LatestTime := time.Now()
+	if req.LatestTime != 0 {
+		LatestTime = time.UnixMilli(req.LatestTime)
+	}
+	videos, err := l.svcCtx.VideoModel.FindByLatestTime(l.ctx, LatestTime, 10)
+	if err != nil {
+		l.Logger.Infof("find videos by latest time error: %v", err)
+		return
+	}
+	videoRsp := make([]*types.Video, 0, len(videos))
+	for _, v := range videos { // TODO: 优化
+		feedItem := &types.Video{}
+		author := l.svcCtx.UserModel.MustFindOne(l.ctx, v.AuthorID)
+		copier.Copy(feedItem, v)
+		copier.Copy(feedItem.Author, author)
+		feedItem.PlayUrl = l.GetVideoURL(v.PlayUrl)
+		videoRsp = append(videoRsp, feedItem)
+	}
+
+	// 判断是否无视频
+	nextTime := LatestTime.UnixMilli()
+	if len(videos) > 0 {
+		nextTime = videos[0].CreatedAt.UnixMilli()
+	}
+	resp = &types.FeedResp{
+		VideoList: videoRsp,
+		NextTime:  nextTime,
+	}
 	return
+}
+
+func (l *FeedLogic) GetVideoURL(key string) string {
+	mac := qbox.NewMac(l.svcCtx.Config.Oss.AccessKeyId, l.svcCtx.Config.Oss.AccessKeySecret)
+	deadline := time.Now().Add(time.Second * 3600).Unix() //1小时有效期
+	privateAccessURL := storage.MakePrivateURL(mac, l.svcCtx.Config.Oss.Endpoint, key, deadline)
+	return privateAccessURL
 }
