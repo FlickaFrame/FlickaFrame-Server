fb54fe23805fe9170663847c5aad9b766c7ebfcc
KeyonYan <835254227@qq.com>
2023-10-27 14:18:08 +0800

    feat: update comment model

--- internal/model/comment/comment.go
+++ internal/model/comment/comment.go
@@ -9,15 +9,14 @@ import (
 
 type Comment struct {
 	gorm.Model
-	Content       string            `gorm:"type:varchar(255);not null"`  // 评论内容
-	OwnerUID      int               `gorm:"index:idx_userid;not null"`   // 评论者ID
-	TargetCID     int							  `gorm:"index:idx_targetid;not null"` // 回复的评论ID
+	Content       string            `gorm:"not null"`  			               // 评论内容
+	OwnerUID      int               `gorm:"index:idx_owneruid;not null"`   // 评论者ID
+	TargetID      int               `gorm:"index:idx_targetid;not null"`   // 评论目标ID
 	User          user_model.User   `gorm:"-"`
-	VideoID       int               `gorm:"index:idx_videoid;not null"`  // 评论的视频ID
 	Video         video_model.Video `gorm:"-"`
-	ReplayCount   int               `gorm:"default:0"`                   // 回复数
-	LikeCount     int               `gorm:"default:0"`                   // 点赞数
-	Type          int               `gorm:"default:0"`                   // 评论类型(0:评论,1:回复)
+	ReplayCount   int               `gorm:"default:0"`                     // 回复数
+	LikeCount     int               `gorm:"default:0"`                     // 点赞数
+	Type          int               `gorm:"default:0"`                     // 评论类型(0:评论视频,1:回复评论)
 }
 
 func (c *Comment) TableName() string {
