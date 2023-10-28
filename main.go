bacbcdd57540ced376a7267a6a14effe257ce67a
caicandong <1290147055@qq.com>
2023-10-28 15:32:50 +0800

    feat: Cors

--- main.go
+++ main.go
@@ -22,7 +22,10 @@ func main() {
 	var c config.Config
 	conf.MustLoad(*configFile, &c)
 
-	server := rest.MustNewServer(c.RestConf, rest.WithUnauthorizedCallback(xcode.UnAuthorizedCallback))
+	server := rest.MustNewServer(c.RestConf,
+		rest.WithUnauthorizedCallback(xcode.UnAuthorizedCallback),
+		rest.WithCors(),
+	)
 	defer server.Stop()
 
 	ctx := svc.NewServiceContext(c)
