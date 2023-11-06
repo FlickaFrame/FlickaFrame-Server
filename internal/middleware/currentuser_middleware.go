090596d7ec8f42bfa50bba9d8058a0b8f4002109
caicandong <1290147055@qq.com>
2023-11-06 11:40:44 +0800

    bugfix: current user middleware

--- internal/middleware/currentuser_middleware.go
+++ internal/middleware/currentuser_middleware.go
@@ -19,7 +19,10 @@ func (m *CurrentUserMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if len(r.Header.Get("Authorization")) > 0 {
 			//has jwt Authorization
-			authHandler := handler.Authorize(m.secret)
+			authHandler := handler.Authorize(m.secret, handler.WithUnauthorizedCallback(
+				func(w http.ResponseWriter, r *http.Request, err error) {
+					next(w, r) // jwt失效，不做处理
+				}))
 			authHandler(next).ServeHTTP(w, r)
 			return
 		} else {
