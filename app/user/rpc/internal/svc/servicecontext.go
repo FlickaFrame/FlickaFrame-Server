9028b31df485d2dc7b506ba217b70392f691b06e
caicandong <1290147055@qq.com>
2023-11-08 12:31:31 +0800

    init

---
+++ app/user/rpc/internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "github.com/FlickaFrame/FlickaFrame-Server/app/user/rpc/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
